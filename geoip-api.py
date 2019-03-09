7f290392d2814421c7edfd1813b2f55f2bca8233
desquiciador <48189610+desquiciador@users.noreply.github.com>
2019-03-09 20:53:06 -0300

    Update geoip-api.py

--- geoip-api.py
+++ geoip-api.py
@@ -1,18 +1,13 @@
 import requests
 import json
-#url de la api
 apiurl='http://ip-api.com/json/'
-#parametros a obtener
 parametros='city,country,countryCode,isp,org,regionName,lat,lon,timezone,zip'
 data={'fields':parametros}
 def ip_scrap(ip=''):
-    #conectamos con la api
     res=requests.get(apiurl+ip, data=data)
-    #obtenemos y procesamos la respuesta json
     apires=json.loads(res.content)
     return apires
 if __name__ == '__main__':
-    #solicitamos la entrada
     ip=input('Ingrese la dirección IP: ')
 #llamamos la funcion ip_scrap y mostramos resultados
 par=parametros.split(',')
